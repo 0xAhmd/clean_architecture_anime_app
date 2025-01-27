43b0714b16a9c75d3bb446ba976514aff0b13e0a
AhmedHesham <ahmedhesham200987@gmail.com>
2025-01-27 19:39:15 +0200

    Row Modified

--- lib/features/home/presentation/view/widgets/anime_rates.dart
+++ lib/features/home/presentation/view/widgets/anime_rates.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AnimeRates extends StatelessWidget {
-  const AnimeRates({super.key});
+  const AnimeRates({super.key,  this.mainAxisAlignment = MainAxisAlignment.start});
 
+
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 8.0),
-      child: const Row(
-        children: [
+      child: Row(
+        mainAxisAlignment: mainAxisAlignment,
+        children: const [
           Icon(
             FontAwesomeIcons.solidStar,
             color: Colors.amberAccent,
