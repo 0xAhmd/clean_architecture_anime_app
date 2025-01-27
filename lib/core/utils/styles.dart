b57933d12041300e12c03683922cecc14610bbba
AhmedHesham <ahmedhesham200987@gmail.com>
2025-01-27 19:38:47 +0200

    styles updated

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,70 +2,7 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const TextStyle title = TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.bold,
-  );
 
-  static const TextStyle titleMedium = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-  );
-
-  static const TextStyle titleSmall = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-  );
-
-  static const TextStyle body = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle bodyMedium = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle bodySmall = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle bodyExtraSmall = TextStyle(
-    fontSize: 10,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle button = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle buttonSmall = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle buttonExtraSmall = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle caption = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle captionSmall = TextStyle(
-    fontSize: 10,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle captionExtraSmall = TextStyle(
-    fontSize: 8,
-    fontWeight: FontWeight.normal,
-  );
 
   static const TextStyle texyStyle18 = TextStyle(
     fontSize: 18,
