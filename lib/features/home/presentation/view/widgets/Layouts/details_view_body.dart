0790bb534a22aa4f35ebd13cf2672b4293e49cb9
AhmedHesham <ahmedhesham200987@gmail.com>
2025-01-29 00:34:36 +0200

    details_screen finished

--- lib/features/home/presentation/view/widgets/Layouts/details_view_body.dart
+++ lib/features/home/presentation/view/widgets/Layouts/details_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:bookly_app/features/home/presentation/view/widgets/actions_button.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/anime_rates.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/custom_card.dart';
+
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/utils/styles.dart';
+import '../recommended_animes_card.dart';
+import '../recommended_animes_list_View.dart';
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
@@ -15,10 +17,11 @@ class DetailsViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         CustomDetailsViewAppBar(),
+        const SizedBox(height: 28),
         Padding(
           padding: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * 0.27),
-          child: CustomCard(),
+          child: RecommendedCards(),
         ),
         const SizedBox(height: 32),
         Text(
@@ -32,12 +35,27 @@ class DetailsViewBody extends StatelessWidget {
                 fontStyle: FontStyle.italic,
               )),
         ),
-        const SizedBox(height: 8),
+        const SizedBox(height: 12),
         AnimeRates(
           mainAxisAlignment: MainAxisAlignment.center,
         ),
-        const SizedBox(height: 16),
+        const SizedBox(height: 32),
         ActionsButton(),
+        const SizedBox(height: 68),
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.texyStyle16.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        RecommendedAnimesSection(),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bookly_app/core/utils/app_router.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,9 +10,7 @@ void main() {
     statusBarColor: Colors.transparent,
   ));
   runApp(
-    DevicePreview(
-      builder: (context) => BooklyApp(),
-    ),
+    BooklyApp(),
   );
 }
 
