73babf66650ee3133edaf5639b4574ffc7071ffe
AhmedHesham <ahmedhesham200987@gmail.com>
2025-03-23 17:19:38 +0200

    dartz added

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entitie.dart';
+import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-  Future<List<BookEntite>> fetchNewestBooks();
-  Future<BookEntite> fetchFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntite>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntite>>> fetchFeaturedBooks();
 }
