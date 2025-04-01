1f51659e05880c7e3bea429f8db45d459a4401b0
AhmedHesham <ahmedhesham200987@gmail.com>
2025-04-01 16:13:56 +0200

    feat: implement HomeRepo for managing book data with local and remote sources

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,41 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/data_sources/home_local_data_src.dart';
+import 'package:bookly_app/features/home/data/data_sources/home_remote_data_src.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSrc homeRemoteDataSrc;
+  final HomeLocalDataSrc homeLocalDataSrc;
+
+  HomeRepoImpl(
+      {required this.homeRemoteDataSrc, required this.homeLocalDataSrc});
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var cachedBooksList = homeLocalDataSrc.fetchFeaturedBooks();
+      if (cachedBooksList.isNotEmpty) {
+        return Right(cachedBooksList);
+      }
+      var books = await homeRemoteDataSrc.fetchFeaturedBooks();
+      return Right(books);
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      var cachedBooksList = homeLocalDataSrc.fetchNewestBooks();
+      if (cachedBooksList.isNotEmpty) {
+        return Right(cachedBooksList);
+      }
+      var books = await homeRemoteDataSrc.fetchNewestBooks();
+      return Right(books);
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+}
